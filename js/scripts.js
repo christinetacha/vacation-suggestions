e332cf90b695ae0361c15675d7f39423db170385
Christine Tacha <tacha.christine@gmail.com>
2016-01-15 15:54:11 -0800

    add js so you can pick from two destinations

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
     $("form#nextVacation").submit(function(event) {
       var age = parseInt($("input#age").val());
-      var romAvd = $("select#romAdv").val();
+      var romanceOrAdventure = $("select#romanceOrAdventure").val();
       var familyFun = $("select#familyFun").val();
       var skiBeach = $("select#skiBeach").val();
       var shop = $("select#shop").val();
       var casino = $("select#casino").val();
 
-      if (age > 40 && romAdv === "romAdv" && familyFun === "familyFun" && skiBeach === "ski" && shop === "shop" && casino === "yes") {
+      if (age > 40 && romanceOrAdventure === "romance" && familyFun === "family" && skiBeach === "ski" && shop === "shopping" && casino === "yes") {
         $("#destination").empty().append("Lake Tahoe, California");
-        // $("#result").show();
+        $("#result").show();
         $("#seeResult").show();
-      }
+      } else if (age < 40 && romanceOrAdventure === "adventure" && familyFun === "nightlife" && skiBeach === "beaches" && shop === "history" && casino === "no") {
+        $("#destination").empty().append("Maui, Hawaii");
+        $("#result").show();
+        $("#seeResult").show();
+      } 
+
 
 
         event.preventDefault();
